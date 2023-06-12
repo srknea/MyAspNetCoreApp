658550a426345c498adf33d80ec265e1ce854b56
Serkan IŞIK <dehnas79@gmail.com>
2023-06-12 20:36:05 +0300

    Remove action methodun oluşturulması - 1

--- MyAspNetCoreApp.Web/Controllers/ProductController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductController.cs
@@ -13,17 +13,24 @@ namespace MyAspNetCoreApp.Web.Controllers
 
             if (!_productRepository.GetAll().Any())
             {
+
                 _productRepository.Add(new() { Id = 1, Name = "Kalem", Price = 10, Stock = 100 });
                 _productRepository.Add(new() { Id = 2, Name = "Defter", Price = 20, Stock = 200 });
                 _productRepository.Add(new() { Id = 3, Name = "Silgi", Price = 30, Stock = 300 });
                 //_productRepository.Add(new Product { Id = 4, Name = "Kalemtraş", Price = 40, Stock = 400 });
             }
-    }
+        }
 
         public IActionResult Index()
         {
             var products = _productRepository.GetAll();
             return View(products);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            _productRepository.Remove(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
