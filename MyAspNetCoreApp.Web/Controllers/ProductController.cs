c0830d5896f426ef9e48985cf631d9d26a1447a7
Serkan IŞIK <dehnas79@gmail.com>
2023-06-15 10:58:40 +0300

    Model Binding - 4

--- MyAspNetCoreApp.Web/Controllers/ProductController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductController.cs
@@ -49,10 +49,8 @@ namespace MyAspNetCoreApp.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult SaveProduct(string Name, decimal Price, int Stock, string Color)
+        public IActionResult SaveProduct(Product newProduct)
         {
-            Product newProduct = new Product() {Name = Name, Price = Price, Stock = Stock, Color = Color };
-
             _context.Products.Add(newProduct);
 
             _context.SaveChanges();
