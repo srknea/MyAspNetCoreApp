07da3ee1796a8cc48b0f8dd167bd628326a57815
Serkan IŞIK <dehnas79@gmail.com>
2023-08-28 14:13:49 +0300

    Required - 2

--- MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -5,14 +5,27 @@ namespace MyAspNetCoreApp.Web.ViewModels
     public class ProductViewModel
     {
         public int Id { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "İsim alanı boş olamaz !")]
         public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
+        
+        [Required(ErrorMessage = "Fiyat alanı boş olamaz !")]
+        public decimal? Price { get; set; }
+
+        [Required(ErrorMessage = "Stok alanı boş olamaz !")]
+        public int? Stock { get; set; }
+
+        [Required(ErrorMessage = "Açıklama alanı boş olamaz !")]
         public string? Description { get; set; }
+        
+        [Required(ErrorMessage = "Renk alanı boş olamaz !")]
         public string Color { get; set; }
-        public DateTime PublishDate { get; set; }
+        
+        [Required(ErrorMessage = "Yayınlanma tarihi boş olamaz !")]
+        public DateTime? PublishDate { get; set; }
         public bool IsPublish { get; set; }
-        public int Expire { get; set; }
+        
+        [Required(ErrorMessage = "Yayında kalma süresi boş olamaz !")]
+        public int? Expire { get; set; }
     }
 }
