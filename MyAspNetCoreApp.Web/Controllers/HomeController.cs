5ab6092afc17bebcda9e51d369f285952504da52
Serkan IŞIK <dehnas79@gmail.com>
2023-08-31 10:31:49 +0300

    Partial View Model Binding - 2

--- MyAspNetCoreApp.Web/Controllers/HomeController.cs
+++ MyAspNetCoreApp.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
 using System.Diagnostics;
 
 namespace MyAspNetCoreApp.Web.Controllers
@@ -7,19 +8,53 @@ namespace MyAspNetCoreApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext _context;
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var products = _context.Products
+                .OrderByDescending(p => p.Id)
+                .Select(x => new ProductPartialViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Stock = x.Stock
+                })
+                .ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
+
+
             return View();
         }
 
         public IActionResult Privacy()
         {
+            var products = _context.Products
+               .OrderByDescending(p => p.Id)
+               .Select(x => new ProductPartialViewModel
+               {
+                   Id = x.Id,
+                   Name = x.Name,
+                   Price = x.Price,
+                   Stock = x.Stock
+               })
+               .ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
+
             return View();
         }
 
