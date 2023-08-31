3cc0655b1dce43cf29b2be9f7d2f9523d68b9502
Serkan IŞIK <dehnas79@gmail.com>
2023-08-31 12:20:37 +0300

    View Component

---
+++ MyAspNetCoreApp.Web/ViewModels/ProductListComponentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyAspNetCoreApp.Web.ViewModels
+{
+    public class ProductListComponentViewModel
+    {
+        public List<ProductComponentViewModel> Products { get; set; }
+    }
+
+    public class ProductComponentViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MyAspNetCoreApp.Web/Views/Shared/ViewComponents/ProductListViewComponents.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Views.Shared.ViewComponents
+{
+    public class ProductListViewComponents : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ProductListViewComponents(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var products = _context.Products
+                .OrderByDescending(p => p.Id)
+                .Select(x => new ProductComponentViewModel()
+                {
+                    Name = x.Name,
+                    Description = x.Description
+                })
+                .ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListComponentViewModel()
+            {
+                Products = products
+            };
+            return View();
+        }
+    }
+}
